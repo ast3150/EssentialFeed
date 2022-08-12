2e1b893907b6b81e91f1d88b7b4e2de79ddd9799
Alain Stulz <alain@appswithlove.com>
2022-08-12 15:59:39 +0200

    Replace 'RemoteFeedLoader.Result' enum with the pre defined `LoadFeedResult` enum from the Feed module (in preparation to conform to the `FeedLoader` protocol)

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -16,10 +16,7 @@ public final class RemoteFeedLoader {
         case invalidData
     }
     
-    public enum Result: Equatable {
-        case success([FeedItem])
-        case failure(Error)
-    }
+    public typealias Result = LoadFeedResult<Error>
     
     public init(url: URL, client: HTTPClient) {
         self.url = url
@@ -38,5 +35,4 @@ public final class RemoteFeedLoader {
             }
         }
     }
-    
 }
