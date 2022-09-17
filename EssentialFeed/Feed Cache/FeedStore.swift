084486d5e77fe62a7ee9f948957a41caf7ba0e49
Alain Stulz <alain@appswithlove.com>
2022-09-17 17:33:52 +0300

    Move `LocalFeedLoader` (and `FeedStore` collaborator) to their own files in production

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Alain Stulz on 17.09.22.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
