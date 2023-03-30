20b1538b1f3905b8167e63ca324ec2e109c3e3ac
Alain Stulz <alain@appswithlove.com>
2023-03-30 20:17:50 +0200

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alain Stulz on 30.03.23.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alain Stulz on 30.03.23.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
