d278becb346ed477b09072c47f02265de103d942
Alain Stulz <alain@appswithlove.com>
2023-04-28 16:32:25 +0200

    `FeedImageDataLoaderCacheDecorator.init` does not load from loader

---
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -0,0 +1,69 @@
+//
+//  FeedImageDataLoaderCacheDecoratorTests.swift
+//  EssentialAppTests
+//
+//  Created by Alain Stulz on 28.04.23.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
+    private let decoratee: FeedImageDataLoader
+    
+    init(decoratee: FeedImageDataLoader) {
+        self.decoratee = decoratee
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return Task()
+    }
+    
+    private class Task: FeedImageDataLoaderTask {
+        func cancel() {}
+    }
+}
+
+class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
+    
+    func test_init_doesNotLoadImageData() {
+        let loader = LoaderSpy()
+        let _ = FeedImageDataLoaderCacheDecorator(decoratee: loader)
+        
+        XCTAssertTrue(loader.loadedURLs.isEmpty, "Expected no loaded URLs in the loader")
+    }
+    
+    private class LoaderSpy: FeedImageDataLoader {
+        private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        private(set) var cancelledURLs = [URL]()
+        
+        private var completions: [(FeedImageDataLoader.Result) -> Void] {
+            messages.map { $0.completion }
+        }
+        
+        var loadedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+        
+        private struct Task: FeedImageDataLoaderTask {
+            let callback: () -> Void
+            func cancel() { callback() }
+        }
+        
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+            messages.append((url, completion))
+            return Task { [weak self] in
+                self?.cancelledURLs.append(url)
+            }
+        }
+        
+        func complete(with error: NSError, at index: Int = 0) {
+            completions[index](.failure(error))
+        }
+        
+        func complete(with data: Data, at index: Int = 0) {
+            completions[index](.success(data))
+        }
+    }
+}
