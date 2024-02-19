f31d11e16ed40f46f9370c4e06ebeb14c962fe7a
Alain Stulz <alain@appswithlove.com>
2024-02-19 22:09:42 +0100

    Add XCTFail to remind us to call `assert` after recording a snapshot

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,8 @@
+//
+//  ImageCommentCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Alain Stulz on 19.02.2024.
+//
+
+import Foundation
---
+++ EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCommentsSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Alain Stulz on 19.02.2024.
+//
+
+import XCTest
+
+final class ImageCommentsSnapshotTests: XCTestCase {
+
+    func test() {
+        XCTFail("Initial failing test. Replace with your own test.")
+    }
+    
+}
