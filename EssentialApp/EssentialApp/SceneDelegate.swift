448c9fa6095cc7693c027f806920f4038befe4df
Alain Stulz <alain@appswithlove.com>
2024-05-29 17:23:14 +0200

    Cache page results

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -97,7 +97,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         last.map { lastItem in
             let url = FeedEndpoint.get(after: lastItem).url(baseURL: baseURL)
             
-            return { [httpClient] in
+            return { [httpClient, localFeedLoader] in
                 httpClient
                     .getPublisher(url: url)
                     .tryMap(FeedItemsMapper.map)
@@ -107,7 +107,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                             items: allItems,
                             loadMorePublisher: self.makeRemoteLoadMoreLoader(items: allItems, last: newItems.last)
                         )
-                    }.eraseToAnyPublisher()
+                    }
+                    .caching(to: localFeedLoader)
+                    .eraseToAnyPublisher()
             }
         }
     }
