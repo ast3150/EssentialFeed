83941373fd82eea34b3e98ebf0e860abf2507456
Alain Stulz <alain@appswithlove.com>
2024-05-30 12:21:22 +0200

    Prevent loading resources again until a running request completes

--- EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
+++ EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
@@ -12,6 +12,7 @@ import EssentialFeediOS
 final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     private let loader: () -> AnyPublisher<Resource, Error>
     private var cancellable: Cancellable?
+    private var isLoading = false
     var presenter: LoadResourcePresenter<Resource, View>?
     
     init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
@@ -19,18 +20,26 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     }
     
     func loadResource() {
-        presenter?.didStartLoading()
+        guard !isLoading else { return }
         
-        cancellable = loader().sink(
-            receiveCompletion: { [weak self] completion in
-                switch completion {
-                case .finished: break
-                case let .failure(error):
-                    self?.presenter?.didFinishLoading(with: error)
-                }
-            }, receiveValue: { [weak self] resource in
-                self?.presenter?.didFinishLoading(with: resource)
+        presenter?.didStartLoading()
+        isLoading = true
+        
+        cancellable = loader()
+            .handleEvents(receiveCancel: { [weak self] in
+                self?.isLoading = false
             })
+            .sink(
+                receiveCompletion: { [weak self] completion in
+                    switch completion {
+                    case .finished: break
+                    case let .failure(error):
+                        self?.presenter?.didFinishLoading(with: error)
+                    }
+                    self?.isLoading = false
+                }, receiveValue: { [weak self] resource in
+                    self?.presenter?.didFinishLoading(with: resource)
+                })
     }
 }
 
